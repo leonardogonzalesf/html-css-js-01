e5f0602653adda76f3a8d6994b6f659472d0c9f2
Leonardo gonzales <leonardo.gonzales@perumoney.pe>
2023-07-10 15:16:56 -0500

    new project

---
+++ leito.js
@@ -0,0 +1,14 @@
+// Obtenemos una referencia al botón
+// var button = document.querySelector('button');
+
+var seleccion = document.getElementById("hola")
+
+// Agregamos un evento de clic al botón
+// button.addEventListener('click', function() {
+//   // Mostramos un mensaje en la consola del navegador
+//   alert('¡Hiciste clic en el botón!');
+// });
+Selection.addEventListener('click', function() {
+  // Mostramos un mensaje en la consola del navegador
+  alert('¡Hiciste clic en el botón!');
+});
